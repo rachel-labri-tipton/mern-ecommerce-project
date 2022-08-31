5ef9d67a8b9f1caab21cdcb9094dc86038fbc112
Rachel Tipton <RACHELMTIPTON@GMAIL.COM>
2022-08-31 14:13:13 +0200

    setting up header and footer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react' 
-import 
+import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 function App () {
   return (
     <>
-      <h1>Buying Time.</h1>
+      <Header />
+      <main className='py-3 center'>
+        <Container>
+          <h1>Welcome to my shop.</h1>
+        </Container>
+      </main>
+      <Footer />
     </>
   )
 }
